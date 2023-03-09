e8326e8f9a1e89b42091a78ba6aa5c5efff8ed26
Natan Iori <natanbaldo11@gmail.com>
2023-03-09 18:38:38 -0300

    trocado banner por video.mp4

--- src/components/banner-home/index.js
+++ src/components/banner-home/index.js
@@ -1,34 +1,26 @@
-import BannerDefault from '../../images/banner-default.jpg'
+import BannerDefault from '../../images/video-banner.mp4'
 import BannerResponsive from '../../images/banner-responsive.jpg'
 import styled from 'styled-components'
 
 const Banner = () => {
     return (
         <>
-            {/* <div>
-                <Video src={BannerDefault} type="video/mp4" autoPlay muted/>
-            </div> */}
-
             <Div>
-                <picture>
-                    <BannerDesktop srcSet={BannerDefault} media="(min-width:600px)"></BannerDesktop>
-                    <BannerResponse src={BannerResponsive} alt="Imagem" />
-                </picture>
+                <Video src={BannerDefault} type="video/mp4" autoPlay muted/>
+                <BannerResponse src={BannerResponsive} alt="Imagem"/>
             </Div>
         </>
     )
 }
 
-const BannerDesktop = styled.source`
-    width: 100%; 
-    height: 500px;
-    max-width: 1920px;
-`
-
 const BannerResponse = styled.img`
     width: 100%;
     max-height: 500px;
+    display: none;
 
+    @media(max-width: 600px){
+        display: block;
+    }
 `
 
 const Div = styled.div`
@@ -36,6 +28,16 @@ const Div = styled.div`
     height: auto;
     display: flex;
     justify-content: center;
+`
+
+const Video = styled.video`
+    width: 100%;
+    max-width: 1920px;
+    display: block;
+
+    @media(max-width: 600px){
+        display: none;
+    }
 `
 
 export { Banner }
