f3277b0ed31293c9886111c1231b62b3f05c19b0
Natan Iori <natanbaldo11@gmail.com>
2023-03-10 16:36:45 -0300

    atualizados os meios de contato do menu mobile

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -116,7 +116,7 @@ const Nav = () => {
 
                         <LiMobile className='li-mobile'><NavLink style={({ isActive }) => isActive ? activeStyle : undefined} className='nav-link nav-link-mobile' to='/sobre'>Sobre Nós</NavLink></LiMobile>
 
-                        <LiMobile className='li-mobile'><NavLink style={({ isActive }) => isActive ? activeStyle : undefined} className='nav-link nav-link-mobile' to='/blog'>Blog</NavLink></LiMobile>
+                        <LiMobile className='li-mobile'><NavLink style={({ isActive }) => isActive ? activeStyle : undefined} className='nav-link nav-link-mobile' to='/blog'>Novidades</NavLink></LiMobile>
 
                         <LiMobile className='li-mobile li-label'><LabelCheck className='hover-contact label-check label-mob nav-link-moblie'>Planos de Saúde</LabelCheck></LiMobile>
                         <li>
@@ -129,10 +129,10 @@ const Nav = () => {
                             </OptionsMob>
                         </li>
                         <li>
-                            <WppMobBtn target='_blank' href="https://wa.me/551130042222?text=Olá,%20tudo%20bem?%20Vim%20do%20seu%20site:%20Adas%20Saúde%20Seguros,%20e%20gostaria%20de%20fazer%20uma%20cotação%20gratuita!" rel="noopener noreferrer"><BsWhatsapp/>&nbsp;Fale com um consultor</WppMobBtn>
+                            <WppMobBtn target='_blank' href="https://wa.me/5511988165363?text=Olá,%20tudo%20bem?%20Vim%20do%20seu%20site:%20Adas%20Saúde%20Seguros,%20e%20gostaria%20de%20fazer%20uma%20cotação%20gratuita!" rel="noopener noreferrer"><BsWhatsapp/>&nbsp;Fale com um consultor</WppMobBtn>
                         </li>
                         <li className='location-mob'><Location/>&nbsp;&nbsp;São Paulo/SP</li>
-                        <li><AnchorMob href='tel:704-555-0127'><Phone/>(704) 555-0127</AnchorMob></li>
+                        <li><AnchorMob href='tel:11-98816-5363'><Phone/>(11) 98816-5363</AnchorMob></li>
                         <li><AnchorMob href='mailto:contato@adasseguros.com.br'><Mail/>Email: contato@adasseguros.com.br</AnchorMob></li>
                         <SocialLiMob>
                             <SocialMobAnchor target='_blank' rel="noopener noreferrer" href="https://www.facebook.com/people/Adas-Sa%C3%BAde-Seguros/100087908922191/"><FacebookMob className='icons'/></SocialMobAnchor>
